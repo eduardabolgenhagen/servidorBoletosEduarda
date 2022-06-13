1610c49ba5474df24347ffa259529bbb279aafec
eduardabolgenhagen <eduarda_b_campos@estudante.sesisenai.org.br>
2022-06-13 20:16:20 -0300

    init

--- api/boletos.js
+++ api/boletos.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const vizualizarPessoaId = require('./pessoas');
-const {listaBoletos} = require('./listaboletos')
 
 const listaBoletos = [
     {
